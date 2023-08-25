fc521544f843a297a27ce009d3068ba67eb7ede4
Caleb Crouch <cecrouch01@gmail.com>
2023-08-25 17:44:04 -0400

    addition of models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,20 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  //figure out what happens onDelete (maybe even onUpdate)
+});
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  // figure out what happens onDelete (maybe even onUpdate)
+});
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
 module.exports = {
   Product,
