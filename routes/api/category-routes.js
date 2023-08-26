0ecd17606aae4575787119df537e728c61fb93fd
Caleb Crouch <cecrouch01@gmail.com>
2023-08-25 21:05:00 -0400

    addition of functioning get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,15 +2,32 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+//This gets all categories and their associated products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
-
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+//This gets a specific cagetory with it's associated product
+router.get('/:id', async (req, res) => {
+try{
+  const singleCategoryData = await Category.findByPk(req.params.id, {
+    include: [{ model: Product}]
+  });
+  //Talk about if this follows industry standards
+  if (singleCategoryData !== null) {
+  res.status(200).json(singleCategoryData)
+  } else {
+    res.status(400).json('Improper Id')
+  }
+} catch(err) {
+  res.status(500).json(err)
+}
 });
 
 router.post('/', (req, res) => {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try{
+    const productData = await Product.findAll({
+      include: [{model: Category}, {model: Tag, through: ProductTag}]
+    });
+    res.status(200).json(productData)
+  } catch(err) {
+    res.status(500).json(err)
+  };
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  //Talk about whether or not a single GET function for all similar routes.
+  try{
+    const singleProductData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag, through: ProductTag}]
+    });
+    //Talk about if this follows industry standards
+    if (singleProductData !== null) {
+    res.status(200).json(singleProductData)
+    } else {
+      res.status(400).json('Improper Id')
+    }
+  } catch(err) {
+    res.status(500).json(err)
+  };
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
